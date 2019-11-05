d8bb1ac13ace1326e78cc76c37a2a303955d3910
Andrei Yarmukhametov <andyarus@gmail.com>
2019-11-05 22:42:29 +0500

    Init project structure

---
+++ ComputerDatabase/View Controllers/ComputerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ComputerViewController.swift
+//  ComputerDatabase
+//
+//  Created by Andrei Coder on 05/11/2019.
+//  Copyright © 2019 yaav. All rights reserved.
+//
+
+import UIKit
+
+class ComputerViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
---
+++ ComputerDatabase/View Controllers/ComputersViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ComputersViewController.swift
+//  ComputerDatabase
+//
+//  Created by Andrei Coder on 05/11/2019.
+//  Copyright © 2019 yaav. All rights reserved.
+//
+
+import UIKit
+
+class ComputersViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
